0bb2f61c87a2bbf2aa195ab8d94f1a655400ed58
Mr.NoOne <114333116+Chittu13@users.noreply.github.com>
2025-02-09 23:04:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,12 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
         if (body.classList.contains('light-mode')) {
             body.classList.remove('light-mode');
             body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark');
         } else {
             body.classList.remove('dark-mode');
             body.classList.add('light-mode');
+            localStorage.setItem('theme', 'light');
         }
     });
 
-    // Initialize with dark mode
-    body.classList.add('dark-mode');
+    // Initialize with the stored theme or default to dark mode
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        body.classList.add(storedTheme);
+    } else {
+        body.classList.add('dark-mode');
+    }
 });
